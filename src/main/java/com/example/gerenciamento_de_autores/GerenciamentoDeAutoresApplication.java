f591b49504e6fca672fc8329f1ca66690bfb3086
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-18 20:00:16 -0300

    Initial commit

---
+++ src/main/java/com/example/gerenciamento_de_autores/GerenciamentoDeAutoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_autores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAutoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeAutoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamento_de_autores/GerenciamentoDeAutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_autores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeAutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
