69e89048bea54fc1100d101df6c35bcb123f0336
Arthur L Molina <arthurlavagnollimolina@gmail.com>
2026-03-22 22:40:44 -0300

    controller criado

---
+++ src/main/java/com/example/gerenciamento_de_autores/controllers/AutorController.java
@@ -0,0 +1,50 @@
+package com.example.gerenciamento_de_autores.controllers;
+
+import com.example.gerenciamento_de_autores.models.AutorModel;
+import com.example.gerenciamento_de_autores.services.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/autor")
+public class AutorController {
+
+    @Autowired
+    AutorService autorService;
+
+    @GetMapping
+    public ResponseEntity<List<AutorModel>> findAll(){
+        List request = autorService.findAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<AutorModel> buscarPorId(@PathVariable Long id){
+        return autorService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<AutorModel> criarAutor(@RequestBody AutorModel autorModel){
+        AutorModel request = autorService.criarAutor(autorModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(autorModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @PutMapping("/{id}")
+    public AutorModel atualizar(@RequestBody AutorModel autorModel,@PathVariable Long id){
+        return autorService.atualizar(autorModel,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(Long id){
+        autorService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
